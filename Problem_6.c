7d3598d74ddf7a9fed0d7a79fe7383712b866ac3
Antoine Dozois <dozois.a@gmail.com>
2016-11-09 23:38:54 +0100

    Adding function and correcting the data structure

--- Problem_6.c
+++ Problem_6.c
@@ -5,35 +5,67 @@
 #include <stdlib.h>
 
 typedef struct Node Node;
+typedef struct Graph Graph;
 
 struct Node{
     char Name;
-    Node* Edge;
+    int Number;
+    char* Edge;
 };
+
+struct Graph{
+    int Number;
+    Node* Nodes;
+};
+
+void createGraph(Graph* g, int number);
+
+void CreateNode(Node* node, char* s);
 
 void parseEntry(char* entry, Node* g);
 
 int findIndex(Node* edge, int size, char name);
 
 int main(void){
+    char s[100] = "", p[100] = "";
+    int offset= 0, nbrNode = 0;
+    Graph* Graph = NULL;
 
 
+    while(scanf("%s",s) != EOF) {
+        offset = strlen(p);
+        for (int i = 0; i < strlen(s); i++) {
+            p[i + offset] = s[i];
+        }
+        nbrNode++;
+    }
+
+ //   createGraph(Graph,nbrNode);
+
+    printf("GGG");
 
     return 0;
+}
+
+void createGraph(Graph* g, int number){
+    g = (struct Graph*) malloc(sizeof(struct Graph));
+    g->Number = number;
+    g->Nodes = (struct Node*) malloc(sizeof(struct Node));
+}
+
+void CreateNode(Node* node, char* s){
+    parseEntry(s,node);
 }
 
 void parseEntry(char* entry, Node* g){
     int len = (int)strlen(entry);
 
-    if(g == NULL){
-        g = calloc(1,sizeof(Node));
-        g->Name = entry[0];
-        g->Edge = calloc(len-2,sizeof(Node));
-        for(int i = 2; i < len; i++){
-            g->Edge[i-2].Name = entry[i];
-        }
-    } else{
-        //findIndex(g->Edge, len-2,entry[i]);
+    g->Name = entry[0];
+    g->Number = len -2;
+    g->Edge = (char*) malloc(sizeof(char));
+
+    for (int i = 2; i < len; i++) {
+        g->Edge[i-2] = entry[i];
     }
 }
 
@@ -44,3 +76,4 @@ int findIndex(Node* edge, int size, char name){
             return i;
     }
 }
+
